d6aca3beb8b3618d70510d58619a633115a58194
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-03-07 10:49:32 +0100

    Apply spottless

--- src/main/java/de/chojo/nexus/entities/Checksum.java
+++ src/main/java/de/chojo/nexus/entities/Checksum.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) RainbowDashLabs and Contributor
+ */
+
 package de.chojo.nexus.entities;
 
 public record Checksum(String sha1, String sha256, String sha512, String md5) {
